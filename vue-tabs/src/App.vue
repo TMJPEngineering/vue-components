e783c120a8a7243b78367d87479965478d12ff03
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-04-24 14:27:17 +0800

    add vue tabs component

---
+++ vue-tabs/src/App.vue
@@ -0,0 +1,138 @@
+<template>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <tabs>
+          <tab name="Tab 1" selected="true">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 1 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 2">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 2 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 3">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 3 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 4">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 4 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 5">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 5 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 6">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 6 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 7">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 7 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 8">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 8 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 9">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 9 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+          <tab name="Tab 10">
+            <article class="message is-primary">
+              <div class="message-header">
+                <p>Tab 10 Content</p>
+                <button class="delete"></button>
+              </div>
+              <div class="message-body">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. <strong>Pellentesque risus mi</strong>, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum <a>felis venenatis</a> efficitur. Aenean ac <em>eleifend lacus</em>, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+              </div>
+            </article>
+          </tab>
+        </tabs>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Tab from './components/Tab'
+import Tabs from './components/Tabs'
+
+export default {
+    name: 'app',
+
+    components: {
+        Tab,
+        Tabs
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-tabs/src/components/Tabs.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="tabs is-fullwidth is-toggle">
+      <ul >
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+          <a :href="tab.href" v-text="tab.name" @click="selectTab(tab)"></a>
+        </li>
+      </ul>
+    </div>
+    
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'tabs',
+
+    data() {
+        return { tabs: [] }
+    },
+
+    created() {
+        this.tabs = this.$children
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach((tab) => {
+                tab.isActive = (tab.name === selectedTab.name)
+            })
+        }
+    }
+}
+</script>
+
+<style>
+  
+</style>
