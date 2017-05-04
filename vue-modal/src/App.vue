1c7a17a7323f41385241e7abc01b09ed4b519087
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-05-04 16:09:00 +0800

    added plain modal component

---
+++ vue-modal/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <section class="section">
+      <div class="container">
+        <modal class-name="button is-primary" button="Plain modal">
+          <template slot="content">
+            <div class="box">
+              Any other Bulma elements you want
+            </div>
+          </template>
+        </modal>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal'
+
+export default {
+    name: 'app',
+
+    components: {
+        Modal
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-modal/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <button :class="computedClass" v-text="button" @click="toggle()"></button>
+
+    <div class="modal" :class="{ 'is-active' : active }">
+      <div class="modal-background"></div>
+      <div class="modal-content">
+        <slot name="content"></slot>
+      </div>
+      <button class="modal-close" @click="toggle()"></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'modal',
+
+    props: {
+        button: { required: true },
+        className: { required: true }
+    },
+
+    data() {
+        return { active: false }
+    },
+
+    methods: {
+        toggle() {
+            this.active = !this.active
+        }
+    },
+
+    computed: {
+        computedClass() {
+            return this.className ? this.className.split(' ') : []
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
